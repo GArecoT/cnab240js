87980d499fdf3b0b5298aa0f188fa2162e427971
Gabriel Areco <areco23@gmail.com>
2024-08-28 13:59:29 -0400

    FEAT: Adicionar menu de configurações

--- cnab240/src/layouts/MainLayout.vue
+++ cnab240/src/layouts/MainLayout.vue
@@ -3,19 +3,26 @@
     <q-layout view="lHh lpr lFf" container class="pagina-full">
       <q-header class="bg-soft-grey text-dark">
         <div class="q-pa-sm q-pl-md row items-center">
-          <div class="cursor-pointer non-selectable">
+          <q-item dense class="cursor-pointer non-selectable q-px-xs q-py-none">
             Gerador
             <q-menu>
               <q-list dense style="min-width: 100px">
-                <q-item clickable v-close-popup>
+                <q-item to="/" clickable v-close-popup>
                   <q-item-section>Remessa</q-item-section>
                 </q-item>
               </q-list>
             </q-menu>
-          </div>
+          </q-item>
 
-          <div class="q-ml-md cursor-pointer">Configurações</div>
-          <div class="q-ml-md cursor-pointer">Sobre</div>
+          <q-item
+            dense
+            to="/config"
+            class="q-ml-md cursor-pointer q-px-sm q-py-none"
+            >Configurações</q-item
+          >
+          <q-item dense class="q-ml-md cursor-pointer q-px-xs q-py-none"
+            >Sobre</q-item
+          >
         </div>
       </q-header>
 
---
+++ cnab240/src/pages/configPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="q-ma-sm component-full bg-soft-grey">
+    <div class="titulo">Configurações</div>
+    <q-splitter v-model="splitterModel" style="height: 250px">
+      <template v-slot:before>
+        <q-tabs
+          indicator-color="primary"
+          no-caps
+          dense
+          switch-indicator
+          v-model="tab"
+          vertical
+          class="text-right text-dark"
+          align="left"
+        >
+          <q-tab name="contas" class="tab-items">
+            <div class="flex items-center q-gutter-x-md">
+              <q-icon name="sym_o_person" size="sm" /> <span>Contas</span>
+            </div></q-tab
+          >
+        </q-tabs>
+      </template>
+
+      <template v-slot:after>
+        <q-tab-panels
+          v-model="tab"
+          animated
+          swipeable
+          vertical
+          transition-prev="jump-up"
+          transition-next="jump-up"
+        >
+          <q-tab-panel name="contas"> </q-tab-panel>
+        </q-tab-panels>
+      </template>
+    </q-splitter>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const splitterModel = ref(20);
+const tab = ref("contas");
+</script>
+<style lang="scss">
+.tab-items {
+  justify-content: flex-start;
+}
+</style>
